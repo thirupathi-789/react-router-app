5483da8179355195cc185154099e5e2f14127c61
Thirupathi <thirupathipathuri234@gmail.com>
2023-12-18 17:22:21 +0530

    facebook

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,9 @@ const Home = () => {
     return (
       <div className="home-container">
         <img src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png" alt="react" className="image"/>
-        <h1 className="heading">React Js</h1>
-        <p className="paragraph">React Js is an open source javascript library used to build User Interfaces(UI)</p>
-        <p className="paragraph">React Js is created by Facebook</p>
+        <h1 className="heading">React JS</h1>
+        <p className="paragraph">React JS is an open source javascript library used to build User Interfaces(UI)</p>
+        <p className="paragraph">React JS is created by facebook</p>
       </div>
     )
   };
